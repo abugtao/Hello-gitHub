2ea35f8e6756f27edbfc558aee43c75b4116b734
lixingguo <joyliyan@qq.com>
2017-11-01 17:53:53 +0800

    add programme
    添加工程

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by zhanghaitao on 2017/11/1.
+//  Copyright © 2017年 zhanghaitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by zhanghaitao on 2017/11/1.
+//  Copyright © 2017年 zhanghaitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
